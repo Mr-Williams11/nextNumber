a1e39efc5bb89767c746298183fb78ba026df36e
Mr-Williams11 <joelwilliams742@gmail.com>
2023-11-22 14:30:23 +0200

    finished

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-  // Function to display the next element in the array
   function getNextElement(arr) {
-    // Check if the array is empty
     if (arr.length === 0) {
       return null;
     }
 
-    // Use a closure to maintain the state (index)
     let index = 0;
 
     return function () {
       if (index < arr.length) {
         return arr[index++];
       } else {
-        // If we reached the end of the array, reset the index
         index = 0;
         return arr[index++];
       }
     };
   }
 
-  // Example usage
-  const arrayToDisplay = [1, 2, 3, 4, 5];
+  const arrayToDisplay = [17, 4, 86, 43, 80];
   const getNextElementInArray = getNextElement(arrayToDisplay);
 
-  // Attach click event handler using JavaScript
   document.getElementById('showNextButton').addEventListener('click', function() {
     displayNextElement();
   });
